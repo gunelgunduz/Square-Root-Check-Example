e526b9fa72ac877827c49107b9c81ae21862c892
Gunel Gunduz <118392442+gunelgunduz@users.noreply.github.com>
2024-08-07 03:04:35 +0400

    Square Root Check Example

---
+++ squareRoot.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int number, result;
+     cout<<"Enter a positive number:";
+     cin>>number;
+     if(number<0){
+        cout<<"please do not enter a neqative number or zeri\n";
+     }else{
+        result=sqrt(number); //suqare root
+        result*=result;
+        if(result==number)
+        cout<<"square root of "<<number<< " is an integer\n";
+        else
+        cout<<"The number entered is not integer\n";
+     }
+     return 0;
+}
+
